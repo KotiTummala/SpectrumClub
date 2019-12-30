b9736832d9fbe7c4584323636dec62ef343a89db
Koti Tummala <koteswar.rao@geappliances.com>
2019-12-30 16:58:57 +0530

    Initial Commit

---
+++ SpectrumClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpectrumClub
+//
+//  Created by Koteswar_Rao on 30/12/19.
+//  Copyright © 2019 Koteswar_Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
